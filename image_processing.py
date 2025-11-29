b50fd1c716469a04864f836eb68fbed32d7b52f5
Y.Nijo <twister2341@outlook.com>
2025-11-29 18:57:03 +0000

    Image processing files updated and near finalised
    SRAD and NLM implementation done.

--- image_processing.py
+++ image_processing.py
@@ -1,18 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
-import scipy
+import cv2, math, scipy
 import scipy.ndimage
+from tqdm import tqdm
 
 
 def image_size(image_data: np.ndarray):
     print(f"Image is of shape {image_data.shape}, size {image_data.size}, max value {np.max(image_data)}")
     return image_data.shape
-
-
-def conv_dec_to_bin(dec): # convert decimal value to a binary value
-    pass
-
 
 def bit_plane_slicing(image_data):
     # find the maximum value, and get the rounded up log2 value 
@@ -41,6 +36,9 @@ def bit_plane_slicing(image_data):
 def mean_filter(image_array, window_size=(3, 3), mode="reflect"):
     # if mode = 'reflect', it will mirror the outer values for padding, otherwise mode = 'constant' gives us a chance to pad with a constant. 
     # weights = np.ones(window_size)
+    if len(window_size) == 1:
+        window_size = (window_size, window_size)
+
     weights = np.full(window_size, 1.0/(math.prod(window_size)))
     return scipy.ndimage.convolve(image_array, weights, mode=mode)
 
@@ -72,15 +70,15 @@ def SNR(window):
     return mean / std
 
 
-def _weighted_median(window, weights):
-    window = np.multiply(window, weights)
+def _weighted_median(window, weights, window_size):
+    window = np.multiply(window.reshape(window_size), weights)
     return np.median(window)
 
 def weighted_median_filter(image_array, weights, window_size=(3, 3), mode="reflect"):
     return scipy.ndimage.generic_filter(image_array, 
                                         _weighted_median, 
                                         size=window_size, 
-                                        extra_arguments=(weights,), 
+                                        extra_arguments=(weights, window_size), 
                                         mode=mode)
 
 
@@ -90,13 +88,15 @@ def _adaptive_median(window, window_size, shift, centre_weight, constant):
     distance = np.array([((x[0]-shift)**2 + (x[1]-shift)**2)**(0.5) for x in positions]).reshape(window_size)
     # shift the positions so that 0, 0 is in the centre of the window, and get the distances from 0, 0. 
 
-    k = (constant * np.std(window)) / np.mean(window)  # get a constant to reduce the matrix operations needed
+    epsilon = 1e-13  # avoid zero-divs
+
+    k = (constant * np.std(window)) / (np.mean(window) + epsilon)  # get a constant to reduce the matrix operations needed
     # this doesn't check for zero mean division
 
     weights = np.full(window_size, centre_weight)
     weights = np.subtract(weights, np.multiply(np.full(window_size, k), distance))
 
-    return _weighted_median(window.reshape(window_size), weights)  # pass off the final calculation to the weighted median f. 
+    return _weighted_median(window.reshape(window_size), weights, window_size)  # pass off the final calculation to the weighted median f. 
 
 def adaptive_median_filter(image_array, centre_weight, constant, window_size=(3, 3), mode="reflect"):
     shift = window_size[0] // 2  # find the necessary shift for the centre of the window, floored div 2. 
@@ -112,12 +112,12 @@ def _geometric_mean(window, k):
 
 def geometric_mean_filter(image_array, window_size=(3, 3), mode="reflect"):  # most commonly used to filter out Gaussian noise
     k = 1.0/np.prod(window_size)  # save a bit of processing time by preprocessing the weight.
-    filter = scipy.ndimage.generic_filter(image_array, 
+    _filter = scipy.ndimage.generic_filter(image_array, 
                                           _geometric_mean, 
                                           window_size, 
                                           extra_arguments=(k,),
                                           mode=mode)
-    return filter
+    return _filter
 
 
 def _arithmetic_mean(window, window_prod):
@@ -125,12 +125,12 @@ def _arithmetic_mean(window, window_prod):
 
 def arithmetic_mean_filter(image_array, window_size=(3, 3), mode="reflect"):
     _window_prod = np.prod(window_size)  # save a bit of processing time by preprocessing the product of the window dims.
-    filter = scipy.ndimage.generic_filter(image_array, 
+    _filter = scipy.ndimage.generic_filter(image_array, 
                                           _arithmetic_mean, 
                                           window_size, 
                                           extra_arguments=(_window_prod,), 
                                           mode=mode)
-    return filter
+    return _filter
 
 
 def histogram_representation(image_array):
@@ -150,22 +150,245 @@ def histogram_representation(image_array):
     plt.show()
 
 
-def histogram_equalisation(image_array):
-    hist, _ = np.histogram(image_array.flatten(), 256, [0, 256])
-    cdf = hist.cumsum()
+def histogram_equalisation(image_array, h_range=255):
+    hist, _ = np.histogram(image_array.flatten(), 256, [0, 256])  # generate a histogram
+    cdf = hist.cumsum() # get the cumulative sum of the histogram
     # cdf_normalized = cdf * float(hist.max()) / cdf.max()
 
     cdf_m = np.ma.masked_equal(cdf, 0)
-    cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+    cdf_m = (cdf_m - cdf_m.min()) * h_range / (cdf_m.max() - cdf_m.min())  # scale the values depending on the range of initial values
     cdf = np.ma.filled(cdf_m, 0).astype('uint8')
     new_image_array = cdf[image_array]
 
     return new_image_array
 
-
 def gaussian_filter(image_array, sigma=1, mode="reflect"):
     return scipy.ndimage.gaussian_filter(image_array, sigma, mode=mode)
 
+def threshold_low_filter(image_array, minVal):
+    image_array[image_array <= minVal] = 0 
+    return image_array
 
-# gaussian lowpass filter
+def _nlm(window, small_window, big_window, Nw):
+
+    half_width = big_window//2
+    window = window.reshape((big_window, big_window, small_window, small_window))
+    # small_window = window[half_width - small_window: half_width + small_window, half_width - small_window: half_width + small_window]
+    local_window = window[half_width, half_width]
+    Ip = evaluate_norm_nlm(local_window, window, Nw)
+    
+    return max(min(255, Ip), 0) # Clipping the pixel values to stay between 0-255 
+
+def non_local_means_filter(image_array, h, small_window, big_window, mode="reflect"):
+
+    # THIS ISN'T USED BECAUSE IT MEMORY LEAKS OR TAKES WAY TOO MUCH MEMORY AND I DECIDED THAT USING A NAIVE SOLUTION WOULD SAVE TIME
+
+    window_size = (big_window, big_window, small_window, small_window)
+    Nw = (h**2)*(small_window**2)
+    padded_image = np.pad(image_array, pad_width=big_window//2, mode=mode)  # pad the image
+    neighbours = find_neighbours_nlm(padded_image, small_window, big_window, *image_array.shape) 
+    _filter = scipy.ndimage.generic_filter(neighbours, 
+                                          _nlm, 
+                                          size=window_size, 
+                                          extra_arguments=(small_window, big_window, Nw,), 
+                                          mode=mode)
+    return _filter
+
+
+def non_local_means(image_array, h, small_window, big_window, mode="reflect"):
+
+    # refer to this paper for the implementation: https://www.researchgate.net/publication/38294293_Nonlocal_Means-Based_Speckle_Filtering_for_Ultrasound_Images
+
+    pad_width = big_window // 2  # define pad width
+    padded_image = np.pad(image_array, pad_width=pad_width, mode=mode)  # pad the image
+
+    output_image = np.zeros(image_array.shape)  # create output array
+    height, width = image_array.shape
+
+    neighbours = find_neighbours_nlm(padded_image, small_window, big_window, height, width) # preprocessing the neighbors of each pixel
+    Nw = (h**2) * (small_window**2) # calculating neighbourhood window
+
+    for i in tqdm(range(pad_width, pad_width + height), desc="NLM user implementation", leave=True):
+        for j in range(pad_width, pad_width + width):
+            pixel_window = neighbours[i,j]  # (small_window x small_window) array around the target pixel (nested array inside the whole array)
+            neighbour_window = neighbours[(i - pad_width):(i + pad_width + 1) , (j - pad_width):(j + pad_width + 1)]  # (big_window x big_window) 
+            # sliced pixel neighbourhood array around the target pixel
+
+            Ip = evaluate_norm_nlm(pixel_window, neighbour_window, Nw)  # calculating Ip
+            output_image[i - pad_width, j - pad_width] = np.clip(Ip, 0, 255) # keeping the pixel values between 0-255 
+
+    return output_image.astype("uint8")
+
+
+def find_neighbours_nlm(padded_image, small_window,big_window, h, w):  # this function simply stores the neighbour array around each pixel (including the padded area).
+    small_width, big_width = small_window // 2, big_window // 2
+    neighbours = np.zeros((padded_image.shape[0], padded_image.shape[1], small_window, small_window))  # make output array
+
+    # finding the neighbors of each pixel in the original image using the padded image
+    for i in range(big_width, big_width + h):
+        for j in range(big_width, big_width + w):   
+            neighbours[i,j] = padded_image[(i - small_width):(i + small_width + 1), (j - small_width):(j + small_width + 1)]
+    
+    return neighbours
+
+# function to calculate the weighted average value (Ip) for each pixel
+def evaluate_norm_nlm(pixel_window, neighbour_window, Nw):  # optimised eval function using only numpy functions instead of python loops
+    squared_diff = (neighbour_window - pixel_window)**2  # find the squared difference between each neighbouring pixel array 
+    # and the original pixel array around the target pixel
+    w = np.exp(-1*(np.sum(squared_diff, axis=(2, 3)))/Nw)  # sum the subarrays elementwise, 
+    # so each subarray is summed but not the outer array (THIS WAS HARD TO FIGURE OUT, FUNNILY ENOUGH.)
+    
+    Iq = neighbour_window[:, :, pixel_window.shape[0]//2, pixel_window.shape[1]//2]  # find all central values 'Iq' of the neighbours
+
+    Ip = np.sum(np.multiply(w, Iq))  # calculate Ip
+    Z = np.sum(w)  # calculate Z
+    return Ip/Z  # return final value
+
+
+def _ad(window, window_size, k, gamma):
+
+    window = window.reshape(window_size)
+
+    centre_x, centre_y = window.shape[0] // 2, window.shape[1] // 2
+
+    deltaN = window[centre_x, centre_y - 1] - window[centre_x, centre_y]
+    deltaS = window[centre_x, centre_y + 1] - window[centre_x, centre_y]
+    deltaE = window[centre_x + 1, centre_y] - window[centre_x, centre_y]
+    deltaW = window[centre_x - 1, centre_y] - window[centre_x, centre_y]
+    
+    gN = np.exp(-(deltaN / k) ** 2.)
+    gS = np.exp(-(deltaS / k) ** 2.)
+    gE = np.exp(-(deltaE / k) ** 2.)
+    gW = np.exp(-(deltaW / k) ** 2.)
+
+    return window[centre_x, centre_y] * (1 - gamma * (gN + gS + gE + gW)) + (gamma * (gN * window[centre_x, centre_y - 1] + gS * window[centre_x, centre_y + 1] + gE * window[centre_x + 1, centre_y] + gW * window[centre_x - 1, centre_y]))
+   
+def anisotropic_diffusion_filter(image_array, k, gamma, window_size=(3, 3), mode="reflect"):
+    _filter = scipy.ndimage.generic_filter(image_array.astype("float32"), 
+                                          _ad, 
+                                          size=window_size, 
+                                          extra_arguments=(window_size, k, gamma,), 
+                                          mode=mode)
+    return _filter
+
+
+def _lee(window, var_k, ):
+
+    """Refer to the paper about SRAD for this implementation."""
+
+    mean = np.mean(window)
+    w = var_k / (var_k + np.var(window))
+    
+    centre_value = window[window.shape[0]//2]
+
+    return mean + w * (centre_value - mean)
+
+def lee_filter(image_array, window_size=(3, 3), mode="reflect"):
+    var_k = np.var(image_array)
+    _filter = scipy.ndimage.generic_filter(image_array.astype("float32"), 
+                                          _lee, 
+                                          size=window_size, 
+                                          extra_arguments=(var_k,), 
+                                          mode=mode)
+    return _filter
+
+
+
+def find_neighbours_srad(image_array, pad_width=1, mode="reflect"):
+        padded_image = np.pad(image_array, pad_width=pad_width, mode=mode)  # pad the image
+        return (
+            padded_image[2:, 1:-1],    # south
+            padded_image[:-2, 1:-1],   # north
+            padded_image[1:-1, 2:],    # east
+            padded_image[1:-1, :-2],   # west
+        )  # return the neighbours, refer to equation 28-29
+
+
+def compute_variation_coefficient(image_array, epsilon=1e-12):
+    nS, nN, nE, nW = find_neighbours_srad(image_array)  # equation 28, 29
+
+    dS = nS - image_array
+    dN = nN - image_array
+    dE = nE - image_array
+    dW = nW - image_array
+
+    gradient = np.sqrt(np.multiply(dS, dS) + np.multiply(dN, dN) + np.multiply(dE, dE) + np.multiply(dW, dW))   
+    # delta I  (hereon referred to as dI), equation 30.5, the proposition 
+    laplacian = (nS + nN + nE + nW - 4*image_array)  # delta I^2 (hereon referred to as dI^2)
+
+    gradient_norm = np.divide(gradient, (image_array + epsilon))  # dI/I
+    laplacian_norm = np.divide(laplacian, (image_array + epsilon))  # dI^2/I
+
+    # compute initial Q, equation 30 and 31, https://ieeexplore.ieee.org/document/1097762
+
+    numerator = (0.5 * np.multiply(gradient_norm, gradient_norm)) - (np.multiply(laplacian_norm, laplacian_norm) / 16) 
+    denominator = 1 + (0.25 * np.multiply(laplacian_norm, laplacian_norm)) 
+    denominator_sqr = np.multiply(denominator, denominator)
+
+    q = np.sqrt(np.divide(numerator, denominator_sqr))  # eq. 31
+
+    return q
+
+
+
+def srad(image_array, n_iters, time_step, decay_factor, mode="reflect"):
+    """Speckle-Reducing Anisotropic Diffusion. Refer to this paper for the implementation:
+    https://ieeexplore.ieee.org/document/1097762 
+
+    :param image_array: image array
+    :type image_array: np.ndarray
+    :param n_iters: iterations to run the SRAD algorithm
+    :type n_iters: int
+    :param time_step: the time step of the algorithm
+    :type time_step: float
+    :param decay_factor: the decay factor of the SRAD algorithm
+    :type decay_factor: float
+    :param mode: padding mode, defaults to "reflect"
+    :type mode: str, optional
+    :return: output image
+    :rtype: np.ndarray
+    """
+
+    # GENUINELY NIGHTMARISH IMPLEMENTATION
+
+    image_array = image_array.astype("float32")
+
+    nS, nN, nE, nW = find_neighbours_srad(image_array, mode=mode)  # equation 28, 29
+
+    dS = nS - image_array
+    dN = nN - image_array
+    dE = nE - image_array
+    dW = nW - image_array
+
+    q = compute_variation_coefficient(image_array)
+
+    Q_0 = q != 0
+    Q_0 = Q_0.astype('float32')
+
+    epsilon = 1e-12  # to make sure that we have no zero values we add epsilon to original image array I when dividing. 
+
+    t = 0 # starting timestep
+
+    for i in tqdm(range(n_iters), desc="Running SRAD", leave=True):
+
+        q = compute_variation_coefficient(image_array)
+        q_0 = Q_0 * np.exp(-decay_factor*t)
+
+        q_0_sqr = np.multiply(q_0, q_0)
+        q_sqr = np.multiply(q, q)
+        eq_33 = np.divide((q_sqr - q_0_sqr), (np.multiply(q_0_sqr, (1 + q_0_sqr)) + epsilon))
+        c = np.exp(-eq_33)   # coefficients of diffusion, equation 33
+
+        S_c, N_c, E_c, W_c = find_neighbours_srad(c, mode=mode)
+
+        div = (
+            S_c * dS - c * dN +
+            E_c * dE - c * dW
+        )  # equation 58
+
+        image_array = image_array + ((time_step / 4) * div)  # equation 61
+        t += time_step 
+    
+    
+    return np.clip(image_array, 0, 255).astype(np.uint8)  # clip values and return as uint8.
 
--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from pathlib import Path
-from image_processing import (bit_plane_slicing, 
-                              image_size,
-                              mean_filter, 
-                              trimmed_mean_filter,
-                              median_filter,
-                              weighted_median_filter,
-                              adaptive_median_filter,
-                              _adaptive_median,
-                              geometric_mean_filter,
-                              arithmetic_mean_filter,
-                              histogram_equalisation,
-                              histogram_representation,
-                              gaussian_filter)  # import all the filters to be tested
+import image_processing as ip
+from tqdm import tqdm
+
 from PIL import Image
 import numpy as np
 import cv2
 
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import mean_squared_error
+
 import matplotlib.pyplot as plt
+
 
 class ImageProcessor:  
     # custom class to keep some variables together, add filters and their args, and to process the image
@@ -25,30 +19,42 @@ class ImageProcessor:
         self.orig_im = image_array
         self.filters = []
 
-    def add_filter(self, filter, *args, **kwargs):  # stores the filter function, their args and kwargs in a list.
+    def add_filter(self, filter, *args, enforce_uint8=False, **kwargs):  # stores the filter function, their args and kwargs in a list.
         self.filters.append((filter, args, kwargs))
-    
-    def process_image(self, verbose=False):  # applies the filter functions in the stored list 'filters' sequentially. 
-        for f, args, kwargs in self.filters:
-            
-            if verbose:
-                print(f"Processing image with filter {f.__name__}")
 
+        if enforce_uint8:
+            self.enforce_uint8()
+
+    def enforce_uint8(self, immediate=False):
+        if immediate:
+            self.im = self.im.astype("uint8")
+        else:  
+            self.filters.append(("enforce_uint8","",""))
+
+    def fft(self):
+        self.im = np.fft.fft2(self.im)
+
+    def ifft(self):
+        self.im = np.fft.ifft2(self.im)
+
+    def __len__(self):
+        return 1
+
+    def process_image(self):  # applies the filter functions in the stored list 'filters' sequentially. 
+
+        pbar = tqdm(self.filters, desc='Processing image with filter: ', leave=True)
+
+        for f, args, kwargs in pbar:
+            if f == "enforce_uint8":
+                self.im = self.im.astype("uint8")
+                continue
+
+            pbar.set_description(f"Processing image with filter: {f.__name__}")
             self.im = f(self.im, *args, **kwargs)
-
         return self.im
 
 
 def read_image(filepath: Path):
-    # match filepath.suffix:
-    #     case ".png":
-    #         pass
-
-    #     case ".jpg":
-    #         pass
-
-    #     case ".pgm":
-    #         pass
     
     image = Image.open(filepath)
     image_array = np.asarray(image)
@@ -56,58 +62,183 @@ def read_image(filepath: Path):
     return image_array 
 
 
-def display_image(image_data):  # show the images side by side. 
+def display_images(image_data, rows, cols):  # show the images side by side. 
     
-    if isinstance(image_data, (list, tuple)):
-        fig, ax = plt.subplots(nrows=1, ncols=len(image_data))
+    fig = plt.figure()
+    ax = []
 
-        for i, axi in enumerate(ax.flat):
-            axi.imshow(image_data[i], alpha=1)
-    else:
-        imgplot = plt.imshow(image_data)
+    for data, i in zip(image_data, range(cols*rows)):
+        # create subplot and append to ax
+        stats = {}
+
+        if len(data) == 1 or isinstance(data, np.ndarray):  # setting the titles and data
+            title = ""
+            image = data
+        else:
+            image, title, *_ = data
+
+            if _:
+                stats = _[0] 
+
+        if isinstance(image, ImageProcessor):  # unwrap if direct
+            image = image.im    
     
+        ax.append(fig.add_subplot(rows, cols, i+1))
+        ax[-1].set_title(str(title))  # set title
+        ax[-1].set_xticks([])  # clear ticks
+        ax[-1].set_yticks([])
+
+        if stats:  # do some comparisons with SSIM and MSE, not great as we don't have a perfect noiseless copy to compare to. 
+            mse_1, mse_2, ssim_1, ssim_2 = *stats["mse"], *stats["ssim"]
+            mse = mean_squared_error(mse_1, mse_2)
+            _ssim = ssim(ssim_1, ssim_2, data_range=ssim_2.max() - ssim_2.min())
+            ax[-1].set_xlabel(f"MSE: {mse:.2f}, SSIM: {_ssim:.2f}")
+
+        plt.imshow(image, alpha=1)
+
     plt.show()  # show the images
 
 
-def main():
-    images = []
-
-    image_filepath = Path("images/NZjers1.png")  # image filepath
-    # image_filepath = Path("images/foetus.png")  # image filepath
-
-    image_array = read_image(image_filepath)  # read the image and save into a numpy array
-    # image_size(image_array)  # test function for displaying imsize
-    images.append(image_array)  # store the original image in a list
-
-    # histogram_representation(stage_2.im)
-
-    for h, tws, sws in [(18, 7, 21),]:
-        img = ImageProcessor(image_array)
-        img.add_filter(cv2.fastNlMeansDenoising, h=h, templateWindowSize=tws, searchWindowSize=sws)
-        img.add_filter(mean_filter)
-        img.process_image(verbose=True)
-        images.append(img.im)
-
-
-    img_canny = ImageProcessor(images[-1])
-    img_canny.add_filter(cv2.Canny, 230, 500)
+def generate_edges(image_data, thr1=230, thr2=500):
+    img_canny = ImageProcessor(image_data)
+    img_canny.add_filter(cv2.Canny, thr1, thr2)
     img_canny.process_image()
-    images.append(img_canny.im)
-    # h=18, tws=7, sws=21
-
-    # cv2_impl = ImageProcessor(image_array)
-    # cv2_impl.add_filter(cv2.fastNlMeansDenoising, h=15, templateWindowSize=7, searchWindowSize=21)
-    # cv2_impl.process_image()
+    return img_canny.im
 
 
-    display_image(images)  # display the images for easy review. 
+def test_nzjers():
+    
+    image_filepath = Path("images/NZjers1.png")  # image filepath
+    original_image = read_image(image_filepath)  # read the image and save into a numpy array
+    images = [[original_image, "original image "]]  # keep images and metadata in list to display at the end
 
-    # ret = bit_plane_slicing(image_array)
-    # for i in ret:
-    #     i
-    #     display_image(i)
+    w = np.full((5, 5), 0.1)  # weights array
+    w[1:4, 1:4] = 0.1 
+    w[2, 2] = 1.
+    
+    test_filters = [
+        (ip.mean_filter, (), {"window_size": (3,3)}),
+        (ip.median_filter, (), {}),
+        (ip.arithmetic_mean_filter, (), {}),
+        (ip.trimmed_mean_filter, (), {}),
+        (ip.weighted_median_filter, (), {"window_size": (5, 5), "weights": w}),
+        (ip.adaptive_median_filter, (1, 0.5), {}),
+        (ip.threshold_low_filter, (50,), {}),
+        (ip.non_local_means, (), {"h":17, "small_window":7, "big_window": 21}),
+                    ]
 
+    for _filter, args, kwargs in test_filters:
+        im = ImageProcessor(original_image.copy())
+        im.add_filter(_filter, *args, **kwargs)
+        im.process_image()
+
+        ksize = (3, 3) if "window_size" not in kwargs else kwargs["window_size"]
+        images.append([im.im, f"{_filter.__name__}, kernel size: {ksize}"])
+
+    display_images(images, rows=3, cols=3)
+
+
+def main_nzjers():
+    images = []  # keep images and metadata in list to display at the end
+    image_filepath = Path("images/NZjers1.png")  # image filepath
+    original_image = read_image(image_filepath)  # read the image and save into a numpy array
+
+    nonlocal_image = ImageProcessor(original_image.copy())
+    nonlocal_image.add_filter(ip.non_local_means, h=17, small_window=7, big_window=21)  # filters are added and run with .process_image()
+    nonlocal_image.add_filter(ip.adaptive_median_filter, 1, 0.001)
+    nonlocal_image.add_filter(ip.threshold_low_filter, minVal=40)  # the args and kwargs are passed onto the filter functions. 
+    
+    nonlocal_image.process_image()
+    
+    w = np.full((5, 5), 0.1)
+    w[1:4, 1:4] = 0.1 
+    w[2, 2] = 1.
+    # print(w)
+
+    gaussian_image = ImageProcessor(nonlocal_image.im)
+    gaussian_image.add_filter(ip.gaussian_filter, sigma=0.95)
+    gaussian_image.process_image()
+
+    thresholded_image = gaussian_image.im.copy()
+    thresholded_image[30 < thresholded_image] = 255  # everything above 30 is considered part of the image
+
+    images.append([original_image, "Original image", {"mse": [original_image, original_image], "ssim": [original_image, original_image]}])
+    images.append([nonlocal_image, "NLM-filtered image + thresholding (pixel < 40 = 0)", {"mse": [original_image, nonlocal_image.im], "ssim": [original_image, nonlocal_image.im]}])
+    images.append([gaussian_image, "Gaussian blurred image", {"mse": [original_image, gaussian_image.im], "ssim": [original_image, gaussian_image.im]}])
+    images.append([thresholded_image, "Thresholded image (pixel > 30 = 255)"])
+    images.append([generate_edges(thresholded_image, thr1=80, thr2=500), "Canny edge detection"])
+
+    tmp = ImageProcessor(original_image)
+    tmp.add_filter(ip.non_local_means, h=17, small_window=7, big_window=21)  # filters are added and run with .process_image()
+    tmp.add_filter(ip.adaptive_median_filter, 1, 0.015)
+    tmp.add_filter(ip.threshold_low_filter, minVal=40)  # the args and kwargs are passed onto the filter functions. 
+    tmp.add_filter(ip.gaussian_filter, sigma=0.95)
+    tmp.process_image()
+    tmp.im[30 < tmp.im] = 255
+    images.append([generate_edges(tmp.im, thr1=80), "with adaptive median filter"])
+
+    display_images(images, rows=2, cols=3)  # display the images for easy review. 
+
+
+def test_foetus():
+    image_filepath = Path("images/foetus.png")  # image filepath
+    original_image = read_image(image_filepath)  # read the image and save into a numpy array
+    images = [[original_image, "original image "]]  # keep images and metadata in list to display at the end
+
+    w = np.full((5, 5), 0.1)  # weights array
+    w[1:4, 1:4] = 0.1 
+    w[2, 2] = 1.
+    
+    test_filters = [
+        (ip.mean_filter, (), {"window_size": (3,3)}),
+        (ip.median_filter, (), {}),
+        (ip.arithmetic_mean_filter, (), {}),
+        (ip.trimmed_mean_filter, (), {}),
+        (ip.weighted_median_filter, (), {"window_size": (5, 5), "weights": w}),
+        (ip.adaptive_median_filter, (1, 0.5), {}),
+        (ip.threshold_low_filter, (50,), {}),
+        (ip.non_local_means, (), {"h":17, "small_window":7, "big_window": 21}),
+                    ]
+
+    for _filter, args, kwargs in test_filters:
+        im = ImageProcessor(original_image.copy())
+        im.add_filter(ip.histogram_equalisation)
+        im.add_filter(_filter, *args, **kwargs)
+        im.process_image()
+
+        ksize = (3, 3) if "window_size" not in kwargs else kwargs["window_size"]
+        images.append([im.im, f"{_filter.__name__}, kernel size: {ksize}"])
+
+    display_images(images, rows=3, cols=3)
+
+
+
+def main_foetus():
+    images = []
+    image_filepath = Path("images/foetus.png")  # image filepath
+    original_image = read_image(image_filepath)
+    images.append(original_image)
+
+    test = ImageProcessor(original_image)
+
+    test.add_filter(ip.srad, 200, 0.2, 0.75)
+    test.add_filter(ip.non_local_means, h=17, small_window=7, big_window=21)
+    # test.add_filter(ip.histogram_equalisation)
+    # for i in range(10):  # niter of anisotropic diff. 
+    #     test.add_filter(ip.anisotropic_diffusion_filter, 10, 0.25)
+    
+    test.process_image()
+    test.enforce_uint8(immediate=True)
+
+    images.append(generate_edges(test.im, thr1=20, thr2=70))
+    images.append(test.im)
+    images.append(generate_edges(test.im, thr1=60, thr2=100))
+    
+    display_images(images, rows=2, cols=2)
 
 
 if __name__ == "__main__":
-    main()
+    # main_nzjers()
+    # test_nzjers()
+    # test_foetus()
+    main_foetus()
